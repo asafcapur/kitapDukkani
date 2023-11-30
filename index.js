468bede9a86ca52530bac14d2ded7b6151727324
asafcapur <asafcapur@gmail.com>
2023-11-30 10:39:30 +0300

    mongodbdersi

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+const Book = require('./models/Book')
+
+// 
+
+app.get('/', (req, res) => {
+    res.status(201).send("hello world")
+})
+
+
+app.post('/books', async (req, res) => {
+
+    console.log(req.body);
+
+    const newBook = { title: req.body.title, author: req.body.author }
+    console.log(newBook);
+
+    const book = await Book.create(newBook)
+    return res.status(201).send({ message: "Başarılı", data: book })
+
+})
+//console.log(process.env.MONGO_URI)
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(
+        app.listen(port, () => {
+            console.log("sunucu çalışmaya başladı....." + port);
+        })
+    )
+    .catch((error) => {
+        console.log(error);
+    })
+
