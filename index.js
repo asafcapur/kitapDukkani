60d5916377cf3cb747e0c1013ba58a3a018d9f02
asafcapur <152477448+asafcapur@users.noreply.github.com>
2023-12-13 10:25:18 +0300

    Put İle  Kitap Uptade DELETE ile Kitap Silme

--- index.js
+++ index.js
@@ -1,93 +1,24 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
-
+const connect = require('./db/connect_mongoose')
+const books = require('./routes/books')
 require('dotenv').config()
 
 const port = process.env.PORT || 3000
 
+// middleware
 app.use(express.json())
+app.use('/api/v1/books', books)
 
-const Book = require('./models/Book')
-
-// 
-
-app.get('/', (req, res) => {
+const start = async () => {
     try {
-
-        res.status(201).send("hello world")
-
-    } catch (error) {
-        return res.status(500).send({ message: error.message })
-    }
-})
-
-
-
-app.post('/books', async (req, res) => {
-
-    try {
-
-        if (!req.body.title || !req.body.author) {
-            return res.status(404).send({ message: "Hata! title ve author gerekli" })
-        }
-
-        const newBook = { title: req.body.title, author: req.body.author, publishYear: req.body.publishYear }
-        const book = await Book.create(newBook)
-        return res.status(201).send({ message: "Başarılı", data: book })
-
-    } catch (error) {
-
-        return res.status(500).send({ message: error.message })
-
-    }
-
-})
-
-app.get('/books', async (req, res) => {
-
-    try {
-
-        const books = await Book.find({});
-        return res.status(201).send({ message: "Başarılı", data: books, count: books.length })
-
-    } catch (error) {
-
-        return res.status(500).send({ message: error.message })
-
-    }
-
-})
-
-app.get('/books/:id', async (req, res) => {
-
-    const id = req.params.id
-    // const {id, elma} = req.params
-
-    console.log(id);
-
-
-    try {
-
-        const book = await Book.findById(id)
-        return res.status(201).send({ message: "Başarılı", data: book })
-
-
-    } catch (error) {
-
-        return res.status(500).send({ message: error.message })
-
-    }
-
-})
-
-
-mongoose.connect(process.env.MONGO_URI)
-    .then(
+        await connect(process.env.MONGO_URI);
         app.listen(port, () => {
-            console.log("sunucu çalışmaya başladı Kardeşım....." + port);
+            console.log("sunucu çalışmaya başladı....." + port);
         })
-    )
-    .catch((error) => {
+    } catch (error) {
         console.log(error);
-    })
+    }
+}
+
+start()
