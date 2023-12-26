b752a50535017114b8172daef7c685460ff874c4
asafcapur <152477448+asafcapur@users.noreply.github.com>
2023-12-26 17:37:25 +0300

    uptade1

--- models/Book.js
+++ models/Book.js
@@ -3,33 +3,34 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: [true, "Kitap adı boş bırakılamaz"],
+        required: [true, "Bir Şeyler Yanlış Lütfen Kitap Adını Boş Bırakmadığınızdan Emin Olup Tekrar Deneyin"],
         maxlength: [100, "Kitap adı en fazla 100 karakter olabilir"],
     },
     author: {
         type: String,
-        required: [true, "Yazar adı boş bırakılamaz"],
+        required: [true, "Bir Şeyler Yanlış Lütfen Yazarı Boş Bırakmadığınızdan Emin Olup Tekrar Deneyin"],
         maxlength: [80, "Yazar adı en fazla 80 karakter olabilir"],
     },
     publishYear: {
         type: Number,
+        required: [true, "Birşeyler Yanlış Lütfen Publısh Yearı Boş Bırakmadığınızdan Emin Olup Tekrar Deneyin"],
         min: [2000, "Yayın yılı en az 2000 olabilir"],
         max: [2023, "Yayın yılı en fazla 2023 olabilir"],
     },
     publisher: {
         type: String,
-        required: [true, "Bir Şeyler Yanlış Lütfen Yayın Evini Boş Bırakmadığınızdan Emin Olun"],
+        required: [true, "Bir Şeyler Yanlış :( Lütfen Yayın Evini Boş Bırakmadığınızdan Emin Olup Tekrar Deneyin"],
         maxlength: [100, "Yayınevi en fazla 100 karakter olabilir"],
     },
     stock: {
         type: Number,
-        required: [true, "Stok miktarı boş bırakılamaz"],
+        required: [true, "Bir Şeyler Yanlış :( Lütfen Stok Miktarını Boş bırakmadığınızdan Emin Olup Tekrar Deneyin"],
         min: [0, "Stok miktarı en az 0 olabilir"],
     },
     price: {
         type: Number,
-        required: [true, "Fiyat boş bırakılamaz"],
-        min: [5, "Fiyat en az 5 TL olabilir"],
+        required: [true, "Bir Şeyler Yanlış :( Lütfen Fiyat Miktarını Boş Bırakmadığınızdan Emin Olun"],
+        min: [5, "Fiyat Miktarı en az 5 TL olabilir"],
     },
     barcode: {
         type: String,
