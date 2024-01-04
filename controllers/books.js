5e1700672d8ba1318325efd55fe3d793aeb3eb3c
asafcapur <152477448+asafcapur@users.noreply.github.com>
2024-01-04 13:21:35 +0300

    lrtc

--- controllers/books.js
+++ controllers/books.js
@@ -1,13 +1,43 @@
 const Book = require('../models/Book')
 
 const getAllBooks = async (req, res) => {
-    const page = Number(req.query.page) || 1
+
+    console.log(req.query);
+
+    // barcode, title, author, publishYear, stock, price, isPublish
+
+
+    // kullanıcın querylerini değişkenlerimize atama
+    const { author, title, publishYear } = req.query
+    const queryObjem = {}
+
+    if (author) {
+        queryObjem.author = author;
+        // queryObjem.selamlama = "naber?"
+    }
+
+    if (title) {
+        //türkçe karakterlerde sıkıntı yaşamayalım diye
+        let modifiedTitle = title.toLocaleLowerCase().replace(/i/gi, '[iİ]').replace(/ı/gi, '[ıI]');
+        //kelime kelime aratabilmek için regex kullandık
+        queryObjem.title = { $regex: modifiedTitle, $options: 'i' };
+    }
+
+    console.log(queryObjem);
+
+    const books = await Book.find(queryObjem)
+
+
+    //skip - atla demek
+    //limit - şu kadar göster demek
+    //query ile page ve limit alıp skip bunlarla hesaplatılıyor
+    /* const page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || 10
     const skip = (page - 1) * limit
 
     console.log({ sayfa: page, limit: limit, atla: skip });
 
-    const books = await Book.find({}).skip(skip).limit(limit);
+    const books = await Book.find({}).skip(skip).limit(limit); */
     return res.status(201).send({ message: "Başarılı", data: books, count: books.length })
 
 }
@@ -21,31 +51,23 @@ const getBook = async (req, res) => {
         return res.status(404).send({ message: "Kitap bulunamadı" })
     }
 
-    return res.status(201).send({ message: "Başarılı Bir Şekilde Yeni Bir Kitap Eklendi!", data: book })
+    return res.status(201).send({ message: "Başarılı", data: book })
 
 }
 
 const addBook = async (req, res) => {
 
-    const newBook = {   title: req.body.title, 
-                        author: req.body.author, 
-                        publishYear: req.body.publishYear,
-                        publisher: req.body.publisher,
-                        stock: req.body.stock,
-                        price: req.body.price,
-                        barcode: req.body.barcode,
-                        promoters: req.body.promoters,
-                        numberOfPages: req.body.numberOfPages,
-                        paperType: req.body.paperType,
-                        lid: req.body.lid,
-                        bookType: req.body.bookType,
-                        paymentOptions: req.body.paymentOptions,
-                        cargoInformation: req.body.cargoInformation,
-                        evaluation: req.body.evaluation,
-                        dimensions: req.body.dimensions
-                    }
+    const newBook = {
+        title: req.body.title,
+        author: req.body.author,
+        publishYear: req.body.publishYear,
+        barcode: req.body.barcode,
+        stock: req.body.stock,
+        price: req.body.price,
+        isPublish: req.body.isPublish,
+    }
     const book = await Book.create(newBook)
-    return res.status(201).send({ message: "Başarılı Bir Şekilde Yeni Bir Kitap Eklendi!", data: book })
+    return res.status(201).send({ message: "Başarılı", data: book })
 
 }
 
@@ -69,7 +91,7 @@ const deleteBook = async (req, res) => {
 
     const book = await Book.findByIdAndDelete(id)
     if (!book) {
-        return res.status(404).send({ message: " Aradğınız Kitap bulunamadı" })
+        return res.status(404).send({ message: "Kitap bulunamadı" })
     }
     return res.status(201).send({ message: "Başarılı", data: book })
 
--- models/Book.js
+++ models/Book.js
@@ -1,93 +1,96 @@
 const mongoose = require('mongoose');
 
-// Bir nesnenin içindeki özellikleri tanımlayan bir obje
-const obje = {
-    // "numberOfPages" adında bir özellik
-    numberOfPages: {
-        // Bu özellik bir sayı tipindedir
-        type: Number,
 
-        // Doğrulama (validation) kurallarını içeren "validate" özelliği
-        validate: {
-            // "validator" özelliği, doğrulama işlevini temsil eder
-            validator: function (value) {
-                // Doğrulama işlevi, belirli bir koşulu kontrol eder
-                // Koşul, value'nun undefined, null veya 1'den büyük olmasıdır
-                return value === undefined || value === null || value >= 1;
-            },
-
-            // "message" özelliği, doğrulama başarısız olduğunda gösterilecek hata mesajını içerir
-            message: "Sayfa sayısını boş bırakmayınız veya 1 veya daha fazla bir değer giriniz."
-        }
-    },
-    // Diğer özellikler buraya eklenebilir
-};
 const bookSchema = new mongoose.Schema({
+    // Kitap Bilgileri
     title: {
         type: String,
-        required: [true, "Kitap adını boş bırakmayınız. Lütfen tekrar deneyin."],
-        maxlength: [100, "Kitap adı en fazla 100 karakter olabilir"],
+        required: [true, "Başlık boş bırakılamaz"],
     },
     author: {
         type: String,
-        required: [true, "Yazarı boş bırakmayınız. Lütfen tekrar deneyin."],
-        maxlength: [80, "Yazar adı en fazla 80 karakter olabilir"],
+        required: [true, "Yazar boş bırakılamaz"],
     },
-    publishYear: {
-        type: Number,
-        required: [true, "Yayın yılını boş bırakmayınız. Lütfen tekrar deneyin."],
-        min: [1000, "Yayın yılı en az 2000 olabilir"],
-        max: [2023, "Yayın yılı en fazla 2023 olabilir"],
-    },
-    publisher: {
+    description: {
         type: String,
-        required: [true, "Yayın evini boş bırakmayınız. Lütfen tekrar deneyin."],
-        maxlength: [100, "Yayınevi en fazla 100 karakter olabilir"],
+        required: false,
+    },
+    category: {
+        type: String,
+        required: [true, "Kategori boş bırakılamaz"],
+    },
+    language: {
+        type: String,
+        required: [true, "Dil boş bırakılamaz"],
+    },
+
+    // Yayın Bilgileri
+    publicationDate: {
+        type: Date,
+        required: [true, "Basım tarihi boş bırakılamaz"]
+    },
+    edition: {
+        type: Number,
+        required: [true, "Baskı boş bırakılamaz"],
+        min: [1, "Baskı numarası en az 1 olabilir"],
+    },
+    placeOfPublication: {
+        type: String,
+        required: [true, "Basım yeri boş bırakılamaz"],
+    },
+
+    // Fiziksel Özellikler
+    width: {
+        type: Number,
+        required: [true, "Genişlik boş bırakılamaz"],
+        min: [1, "Genişlik en az 1 olabilir"]
+    },
+    height: {
+        type: Number,
+        required: [true, "Yükseklik boş bırakılamaz"],
+        min: [1, "Yükseklik en az 1 olabilir"]
+    },
+    pageCount: {
+        type: Number,
+        required: [true, "Sayfa sayısı boş bırakılamaz"],
+        min: [1, "Sayfa sayısı en az 1 olabilir"],
+    },
+    coverType: {
+        type: String,
+        required: [true, "Kapak türü boş bırakılamaz"],
+    },
+    paperType: {
+        type: String,
+        required: [true, "Kağıt türü boş bırakılamaz"],
+    },
+
+    // Stok ve Fiyatlandırma
+    barcode: {
+        type: String,
+        required: [true, "Barkod boş bırakılamaz"],
+        unique: true,
     },
     stock: {
         type: Number,
-        required: [true, "Stok miktarını boş bırakmayınız. Lütfen tekrar deneyin."],
+        required: [true, "Stok miktarı boş bırakılamaz"],
         min: [0, "Stok miktarı en az 0 olabilir"],
     },
     price: {
         type: Number,
-        required: [true, "Fiyat miktarını boş bırakmayınız. Lütfen tekrar deneyin."],
-        min: [5, "Türkiye Şartlarında Bu Kadar Oluyor :("],
+        required: [true, "Fiyat boş bırakılamaz"],
+        min: [0, "Fiyat en az 0 olabilir"],
     },
-    barcode: {
-        type: String,
-        required: [true, "Barkodu boş bırakmayınız."],
-        unique: true,
-    },
-    promoters: {
-        type: [String],
-        required: [true, "Destekleyicileri boş bırakmayınız. Lütfen tekrar deneyin."],
-        maxlength: [100, "Destekleyiciler en fazla 100 karakter olabilir"],
-    },
-    ...obje, // obje içindeki özellikleri buraya ekleyin
-    paperType: {
-        type: String,
-        required: [true, "Kağıt tipini boş bırakmayınız. Lütfen tekrar deneyin."],
-    },
-    lid: {
-        type: String,
-        required: [true, "Kapak bilgisini boş bırakmayınız. Lütfen tekrar deneyin."]
-    },
-    bookType: {
-        type: [String],
-        required: [true, "Kitap türünü boş bırakmayınız. Lütfen tekrar deneyin."],
-    },
-    paymentOptions: {
-        type: [String],
-        required: [true, "Lütfen İlk Önce Ödeme Seçeneğinizi Seçin!"]
-    },
-    evaluation: {
-        type: Number,
-        required: [true, "Lütfen Kitabımıza 10 Üzerinden Bir Puan Verin!"],
-        max: [10, "En Fazla 10 Puan Verebilirsiniz"],
-    },  
-})
 
+    // Diğer Bilgiler
+    image: {
+        type: String,
+        required: false,
+    },
+
+}, {
+    timestamps: true
+});
 
 
 module.exports = mongoose.model('Book', bookSchema);
+
